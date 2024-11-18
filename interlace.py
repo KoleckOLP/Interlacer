d86f8ea8a7d9e8bcb189a8c87c69a6349f492212
koleq(WORK-D) <o.kurz@atlas.cz>
2024-11-18 16:17:15 +0100

    Parralel beast

--- interlace.py
+++ interlace.py
@@ -1,9 +1,11 @@
-import os, glob
+import os, glob, time, subprocess
 from PIL import Image
 
+start = time.time()
+
 magick = "ImageMagick\\magick.exe"  # path to your ImageMagic
-source = "test.png"  # image you want to intelace
-offset = 0.003  # use between 0.001 and 0.003 
+source = "test6.png"  # image you want to intelace
+offset = 0.01  # use between 0.001 and 0.003 
 
 with Image.open(source) as img:  # get size of the image
     width, height = img.size
@@ -15,49 +17,60 @@ if offset < 1:  # if the result is less than 1 force 1
 for i in range(0, height, 2):  # extract even lines
     percentage = round((i / height)*100, 2)
     print(f"{magick} {source} -crop x1+0+{i} even\\line_{str(i).zfill(4)}.png ({percentage}%)")
-    os.system(f"{magick} {source} -crop x1+0+{i} even\\line_{str(i).zfill(4)}.png")
+    subprocess.Popen([magick, source, "-crop", f"x1+0+{i}", f"even\\line_{str(i).zfill(4)}.png"])
 
 for i in range(1, height, 2):  # exctact odd lines
     percentage = round((i / height)*100, 2)
     print(f"{magick} {source} -crop x1+0+{i} all\\line_{str(i).zfill(4)}.png ({percentage}%)")
-    os.system(f"{magick} {source} -crop x1+0+{i} all\\line_{str(i).zfill(4)}.png")
+    subprocess.Popen([magick, source, "-crop", f"x1+0+{i}", f"all\\line_{str(i).zfill(4)}.png"])
 
 even_lines = glob.glob("even\\*.png")  # get a list of even lines
 
 for i, line in enumerate(even_lines):  # offset even lines and crop them
     percentage = round((i / len(even_lines))*100, 2)
-    print(f"{magick} {line} -background white -splice {offset}x0 +repage {line[:-4]}_shift.png ({percentage}%)")
-    os.system(f"{magick} {line} -background white -splice {offset}x0 +repage {line[:-4]}_shift.png")
-    print(f"{magick} {line[:-4]}_shift.png -crop {width}x1+0+0 {line[:-4]}_done.png")
-    os.system(f"{magick} {line[:-4]}_shift.png -crop {width}x1+0+0 {line[:-4]}_done.png")
-
-even_lines_done = glob.glob("even\\*_done.png")  # get a list of finalized even lines
-
-for i, line in enumerate(even_lines_done):  # copy finalized even lines to all directory
-    percentage = round((i / len(even_lines_done))*100, 2)
-    print(f"copy {line} all\\{line[5:-9]}.png ({percentage}%)")
-    os.system(f"copy {line} all\\{line[5:-9]}.png")
+    print(f"{magick} {line} -background white -splice {offset}x0 +repage -crop {width}x1+0+0 all\\{line[4:]}")
+    subprocess.Popen([magick, line, "-background", "white", "-splice", f"{offset}x0", "+repage", "-crop", f"{width}x1+0+0", f"all\\{line[4:]}"])
 
 print(f"del even\\*.png")
 os.system(f"del even\\*.png")  # deleting everything in the even folder, dangerous
 
 all_lines = glob.glob("all\\*png")  # get a list of all lines
 
-current_line = str(all_lines[0])[4:]  # get name of the first line and remove folder name from it
+# Function to split the list into chunks of 10
+def chunk_lines(arr, size=10):
+    return [arr[i:i + size] for i in range(0, len(arr), size)]
 
-for i in range(1, len(all_lines)):  # append all of the pictures together
-    percentage = round((i / len(all_lines))*100, 2)
-    next_line = all_lines[i]
+# Split all lines into chunks of 10 images
+all_lines_chunks = chunk_lines(all_lines, 750)
 
-    result_image = f"temp_{str(i).zfill(4)}.png"
+# Process each chunk in parallel
+for i in range(len(all_lines_chunks)):  # append all of the pictures together
 
-    print(f"{magick} all\\{current_line} {next_line} -append all\\{result_image} ({percentage}%)")
-    os.system(f"{magick} all\\{current_line} {next_line} -append all\\{result_image}")
+    next_line = all_lines_chunks[i]
+
+    result_image = f"all\\temp_{str(i).zfill(4)}.png"
+
+    print(i)
+
+    if i == 0:
+        command = [magick] + next_line + ["-append", result_image]
+    else:
+        command = [magick, current_line] + next_line + ["-append", result_image]
+    
+    # Print the command for debugging
+    print(command)
+    
+    # Run the command in the background
+    subprocess.run(command)
 
     current_line = result_image
 
-print(f"copy all\\temp_{str(height-1).zfill(4)}.png {source[:-4]}_interlaced.png")
-os.system(f"copy all\\temp_{str(height-1).zfill(4)}.png {source[:-4]}_interlaced.png")  # copy the final image next to the original
+print(f"copy {result_image} {source[:-4]}_interlaced.png")
+os.system(f"copy {result_image} {source[:-4]}_interlaced.png")
 
 print(f"del all\\*.png")
 os.system(f"del all\\*.png")  # deleting everything in the all folder, dangerous
+
+end = time.time()
+result = end - start
+print(f"started {start}, ended {end}, took {result}")
